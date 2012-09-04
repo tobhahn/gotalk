9ac652344a4cc8a576688d08791a3770510c8e69
Tobias Hahn <tobhahn@web.de>
2012-09-04 17:47:38 +0200

    Implement compile+run.
    Stolen shamelessly from misc/goplay

--- goplay.go
+++ goplay.go
@@ -2,9 +2,112 @@ package gotalk
 
 import (
 	"bytes"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strconv"
 )
 
+// Compile is an HTTP handler that reads Go source code from the request,
+// runs the program (returning any errors),
+// and sends the program's output as the HTTP response.
 func compile(w http.ResponseWriter, req *http.Request) {
-	w.Write(bytes.NewBufferString("").Bytes())
+	out, err := compileRequest(req)
+	if err != nil {
+		error_(w, out, err)
+		return
+	}
+
+	// write the output of x as the http response
+	w.Write(out)
+}
+
+var (
+	commentRe = regexp.MustCompile(`(?m)^#.*\n`)
+	tmpdir    string
+	// a source of numbers, for naming temporary files
+	uniq = make(chan int)
+)
+
+func init() {
+	// find real temporary directory (for rewriting filename in output)
+	var err error
+	tmpdir, err = filepath.EvalSymlinks(os.TempDir())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// source of unique numbers
+	go func() {
+		for i := 0; ; i++ {
+			uniq <- i
+		}
+	}()
+}
+
+func compileRequest(req *http.Request) (out []byte, err error) {
+	// x is the base name for .go, .6, executable files
+	x := filepath.Join(tmpdir, "compile"+strconv.Itoa(<-uniq))
+	src := x + ".go"
+	bin := x
+	if runtime.GOOS == "windows" {
+		bin += ".exe"
+	}
+
+	// rewrite filename in error output
+	defer func() {
+		if err != nil {
+			// drop messages from the go tool like '# _/compile0'
+			out = commentRe.ReplaceAll(out, nil)
+		}
+		out = bytes.Replace(out, []byte(src+":"), []byte("main.go:"), -1)
+	}()
+
+	// write body to x.go
+	body := new(bytes.Buffer)
+	if _, err = body.ReadFrom(req.Body); err != nil {
+		return
+	}
+	defer os.Remove(src)
+	if err = ioutil.WriteFile(src, body.Bytes(), 0666); err != nil {
+		return
+	}
+
+	// build x.go, creating x
+	dir, file := filepath.Split(src)
+	out, err = run(dir, "go", "build", "-o", bin, file)
+	defer os.Remove(bin)
+	if err != nil {
+		return
+	}
+
+	// run x
+	return run("", bin)
+}
+
+// error writes compile, link, or runtime errors to the HTTP connection.
+// The JavaScript interface uses the 404 status code to identify the error.
+func error_(w http.ResponseWriter, out []byte, err error) {
+	w.WriteHeader(404)
+	if out != nil {
+		w.Write(out)
+	} else {
+		w.Write([]byte(err.Error()))
+	}
+}
+
+// run executes the specified command and returns its output and an error.
+func run(dir string, args ...string) ([]byte, error) {
+	var buf bytes.Buffer
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Dir = dir
+	cmd.Stdout = &buf
+	cmd.Stderr = cmd.Stdout
+	err := cmd.Run()
+	return buf.Bytes(), err
 }
--- goplay_test.go
+++ goplay_test.go
@@ -26,8 +26,11 @@ func (w *probeResponseWriter) Write(data []byte) (int, error) {
 	buf := bytes.NewBuffer(data)
 	w.response += buf.String()
 
-	if buf.Len() > 0 {
-		panic(fmt.Sprintf("Could not append all of data, %v bytes left.", buf.Len()))
+	if buf.Len() != len(data) {
+		panic(
+			fmt.Sprintf(
+				"Internal error: buf.Len() (== %v) is not equal to len(data) (== %v). Response: %v",
+				buf.Len(), len(data), w.response))
 	}
 
 	if w.status == 0 {
@@ -41,7 +44,7 @@ func (w *probeResponseWriter) WriteHeader(statusCode int) {
 	w.status = statusCode
 }
 
-func Test_compile_should_be_able_to_handle_an_empty_request(t *testing.T) {
+func Test_compiling_an_empty_request_should_return_404_compiler_error(t *testing.T) {
 	req, err := http.NewRequest("GET", "/compile", strings.NewReader(""))
 	if err != nil {
 		t.Fatalf("Error creating test request: %v", err)
@@ -50,7 +53,37 @@ func Test_compile_should_be_able_to_handle_an_empty_request(t *testing.T) {
 	var res probeResponseWriter
 	compile(&res, req)
 
-	if res.status != http.StatusOK {
+	if res.status != http.StatusNotFound {
 		t.Errorf("compile failed with status %v", http.StatusText(res.status))
+		t.Errorf("response: %v", res.response)
+	}
+}
+
+var hello_世界 = `
+package main
+
+import ("fmt")
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+`
+
+func Test_compile_hello_世界(t *testing.T) {
+	req, err := http.NewRequest("GET", "/compile", strings.NewReader(hello_世界))
+	if err != nil {
+		t.Fatalf("Error creating test request: %v", err)
+	}
+
+	var res probeResponseWriter
+	compile(&res, req)
+
+	if res.status != http.StatusOK {
+		t.Fatalf("compile failed with status %v", http.StatusText(res.status))
+	}
+
+	if res.response != "Hello, 世界\n" {
+		t.Log(`Expected: Hello, 世界\n`)
+		t.Errorf("Actual: %v", res.response)
 	}
 }
